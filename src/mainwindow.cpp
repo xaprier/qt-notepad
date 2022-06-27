491a086fdee33d88a3aa1d4cc2d34eee1f1dfaac
Seymen Kalkan <69122856+xaprier@users.noreply.github.com>
2022-06-27 16:29:13 +0300

    connect Event and triggers to the functions

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -3,6 +3,33 @@
 
 mainwindow::mainwindow(QWidget *parent) : QMainWindow(parent) , ui(new Ui::mainwindow) {
     ui->setupUi(this);
+<<<<<<< HEAD
+
+    ui -> showLine -> setText(
+                QString::fromStdString(
+                    "Line " + std::to_string(ui -> textEdit -> textCursor().blockNumber() + 1) +
+                    ", Column " + std::to_string(ui -> textEdit -> textCursor().columnNumber() + 1)
+                    )
+                );
+
+    // creating event controls
+    // when the action clicked, do this function...
+    // trigger on click
+    connect(ui -> actionNew, &QAction::triggered, this, &mainwindow::newDocument);
+    connect(ui -> actionOpen, &QAction::triggered, this, &mainwindow::open);
+    connect(ui -> actionSave, &QAction::triggered, this, &mainwindow::save);
+    connect(ui -> actionSave_As, &QAction::triggered, this, &mainwindow::saveAs);
+
+    connect(ui -> actionSelect_Font, &QAction::triggered, this, &mainwindow::selectFont);
+    connect(ui -> actionBold, &QAction::triggered, this, &mainwindow::setFontBold);
+    connect(ui -> actionItalic, &QAction::triggered, this, &mainwindow::setFontItalic);
+    connect(ui -> actionUnderline, &QAction::triggered, this, &mainwindow::setFontUnderLine);
+
+    connect(ui -> actionAbout_Notepad, &QAction::triggered, this, &mainwindow::about);
+
+    // trigger every textEdit change
+    connect(ui -> textEdit, &QTextEdit::cursorPositionChanged, this, &mainwindow::cursorLoc);
+=======
 
     // set our showLine text on first start
     ui -> showLine -> setText(
@@ -12,6 +39,7 @@ mainwindow::mainwindow(QWidget *parent) : QMainWindow(parent) , ui(new Ui::mainw
                     )
                 );
 
+>>>>>>> 5fea135793b2bc3a311de2fc461caff1685ade77
 }
 
 mainwindow::~mainwindow()
@@ -19,8 +47,56 @@ mainwindow::~mainwindow()
     delete ui;
 }
 
+<<<<<<< HEAD
+void mainwindow::open() {
+    return;
+}
+=======
 // override functions of class
+>>>>>>> 5fea135793b2bc3a311de2fc461caff1685ade77
 
+<<<<<<< HEAD
+void mainwindow::newDocument() {
+    return;
+}
+
+void mainwindow::save() {
+    return;
+}
+
+void mainwindow::saveAs() {
+    return;
+}
+
+void mainwindow::selectFont() {
+    return;
+}
+
+void mainwindow::setFontUnderLine(bool underline) {
+    return;
+}
+
+void mainwindow::setFontItalic(bool italic) {
+    return;
+}
+
+void mainwindow::setFontBold(bool bold) {
+    return;
+}
+
+void mainwindow::about() {
+    return;
+}
+
+void mainwindow::closeEvent(QCloseEvent *event) {
+    return;
+}
+
+void mainwindow::cursorLoc() {
+    return;
+}
+
+=======
 void mainwindow::open() {
     return;
 }
@@ -64,3 +140,5 @@ void mainwindow::closeEvent(QCloseEvent *event) {
 void mainwindow::cursorLoc() {
     return;
 }
+
+>>>>>>> 5fea135793b2bc3a311de2fc461caff1685ade77
