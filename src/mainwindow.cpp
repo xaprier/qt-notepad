6cbd7787aebd5e250c94615952f3ab975af00159
Seymen Kalkan <69122856+xaprier@users.noreply.github.com>
2022-06-27 16:31:14 +0300

    connect Event and triggers to the functions

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -3,7 +3,6 @@
 
 mainwindow::mainwindow(QWidget *parent) : QMainWindow(parent) , ui(new Ui::mainwindow) {
     ui->setupUi(this);
-<<<<<<< HEAD
 
     ui -> showLine -> setText(
                 QString::fromStdString(
@@ -29,17 +28,6 @@ mainwindow::mainwindow(QWidget *parent) : QMainWindow(parent) , ui(new Ui::mainw
 
     // trigger every textEdit change
     connect(ui -> textEdit, &QTextEdit::cursorPositionChanged, this, &mainwindow::cursorLoc);
-=======
-
-    // set our showLine text on first start
-    ui -> showLine -> setText(
-                QString::fromStdString(
-                    "Line " + std::to_string(ui -> textEdit -> textCursor().blockNumber() + 1) +
-                    ", Column " + std::to_string(ui -> textEdit -> textCursor().columnNumber() + 1)
-                    )
-                );
-
->>>>>>> 5fea135793b2bc3a311de2fc461caff1685ade77
 }
 
 mainwindow::~mainwindow()
@@ -47,56 +35,6 @@ mainwindow::~mainwindow()
     delete ui;
 }
 
-<<<<<<< HEAD
-void mainwindow::open() {
-    return;
-}
-=======
-// override functions of class
->>>>>>> 5fea135793b2bc3a311de2fc461caff1685ade77
-
-<<<<<<< HEAD
-void mainwindow::newDocument() {
-    return;
-}
-
-void mainwindow::save() {
-    return;
-}
-
-void mainwindow::saveAs() {
-    return;
-}
-
-void mainwindow::selectFont() {
-    return;
-}
-
-void mainwindow::setFontUnderLine(bool underline) {
-    return;
-}
-
-void mainwindow::setFontItalic(bool italic) {
-    return;
-}
-
-void mainwindow::setFontBold(bool bold) {
-    return;
-}
-
-void mainwindow::about() {
-    return;
-}
-
-void mainwindow::closeEvent(QCloseEvent *event) {
-    return;
-}
-
-void mainwindow::cursorLoc() {
-    return;
-}
-
-=======
 void mainwindow::open() {
     return;
 }
@@ -141,4 +79,3 @@ void mainwindow::cursorLoc() {
     return;
 }
 
->>>>>>> 5fea135793b2bc3a311de2fc461caff1685ade77
