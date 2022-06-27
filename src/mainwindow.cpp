5fea135793b2bc3a311de2fc461caff1685ade77
Seymen Kalkan <69122856+xaprier@users.noreply.github.com>
2022-06-27 16:12:39 +0300

    Created override function bodies for handle and set cursor location text on first start

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -3,6 +3,15 @@
 
 mainwindow::mainwindow(QWidget *parent) : QMainWindow(parent) , ui(new Ui::mainwindow) {
     ui->setupUi(this);
+
+    // set our showLine text on first start
+    ui -> showLine -> setText(
+                QString::fromStdString(
+                    "Line " + std::to_string(ui -> textEdit -> textCursor().blockNumber() + 1) +
+                    ", Column " + std::to_string(ui -> textEdit -> textCursor().columnNumber() + 1)
+                    )
+                );
+
 }
 
 mainwindow::~mainwindow()
@@ -10,3 +19,48 @@ mainwindow::~mainwindow()
     delete ui;
 }
 
+// override functions of class
+
+void mainwindow::open() {
+    return;
+}
+
+void mainwindow::newDocument() {
+    return;
+}
+
+void mainwindow::save() {
+    return;
+}
+
+void mainwindow::saveAs() {
+    return;
+}
+
+void mainwindow::selectFont() {
+    return;
+}
+
+void mainwindow::setFontUnderLine(bool underline) {
+    return;
+}
+
+void mainwindow::setFontItalic(bool italic) {
+    return;
+}
+
+void mainwindow::setFontBold(bool bold) {
+    return;
+}
+
+void mainwindow::about() {
+    return;
+}
+
+void mainwindow::closeEvent(QCloseEvent *event) {
+    return;
+}
+
+void mainwindow::cursorLoc() {
+    return;
+}
