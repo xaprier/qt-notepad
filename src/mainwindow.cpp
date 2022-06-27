ec38d9dcadbf69fb1e2f7d90873d1a57c4e5e577
Seymen Kalkan <69122856+xaprier@users.noreply.github.com>
2022-06-27 21:58:52 +0300

    Completed all functions of Text Editor

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -1,5 +1,6 @@
 #include "mainwindow.h"
 #include <QFile>
+#include <QFileDialog>
 #include <QMessageBox>
 #include <QCloseEvent>
 #include <QFontDialog>
@@ -40,19 +41,164 @@ mainwindow::~mainwindow()
 }
 
 void mainwindow::open() {
-    return;
+    QString inText = ui -> textEdit -> toPlainText();
+
+    if (!inText.isEmpty()) {
+        QString fileName = QWidget::windowTitle();
+        QFile file(fileName);
+        QTextStream in(&file);
+
+        if (!file.open(QIODevice::ReadOnly | QFile::Text)) {
+            QMessageBox::warning(this, "Warning", "An error occured:: " + file.errorString());
+            return;
+        }
+
+        QString inText2 = in.readAll();
+
+        if (inText != inText2) {
+            QMessageBox::StandardButton reply;
+            reply = QMessageBox::question(
+                        this,
+                        "Save File",
+                        "File not saved. Save changes before closing?",
+                        QMessageBox::Save | QMessageBox::Cancel | QMessageBox::Close
+                        );
+            if (reply == QMessageBox::Save) {
+                save();
+                currentFile.clear();
+                ui -> textEdit -> clear();
+            } else if (reply == QMessageBox::Close) {
+                currentFile.clear();
+                ui -> textEdit -> clear();
+                goto jmp;
+            } else {
+                return;
+            }
+        } else {
+            currentFile.clear();
+            ui -> textEdit -> clear();
+        }
+    } else {
+        currentFile.clear();
+        ui -> textEdit -> clear();
+        goto jmp;
+    }
+jmp:
+    QString fileName = QFileDialog::getOpenFileName(this, "Open File");
+    if (fileName.isEmpty())
+        return;
+    QFile file(fileName);
+    currentFile = fileName;
+
+    if (!file.open(QIODevice::ReadOnly | QFile::Text)) {
+        QMessageBox::warning(this, "Warning", "Cannot open file: " + file.errorString());
+        return;
+    }
+
+    setWindowTitle(fileName);
+
+    QTextStream in(&file);
+    QString text = in.readAll();
+    ui -> textEdit -> setText(text);
+
+
 }
 
 void mainwindow::newDocument() {
-    return;
+    QString text = ui -> textEdit -> toPlainText();
+    if (!text.isEmpty()) {
+        QString fileName = QWidget::windowTitle();
+        QFile file(fileName);
+        QTextStream in(&file);
+        if (!file.open(QIODeviceBase::ReadWrite | QFile::Text)) {
+            QMessageBox::warning(this, "Warning", "An error occured: " + file.errorString());
+        }
+        QString text2 = in.readAll();
+        if (text != text2) {
+            QMessageBox::StandardButton reply;
+            reply = QMessageBox::question(
+                        this,
+                        "Save File",
+                        "File not saved. Save changes before closing?",
+                        QMessageBox::Save | QMessageBox::Cancel | QMessageBox::Close
+                        /*, QMessageBox::Cancel (for change default button)*/
+                        );
+
+            if (reply == QMessageBox::Save) {
+                save();
+                currentFile.clear();
+                setWindowTitle("Notepad");
+                ui -> textEdit -> clear();
+            } else if (reply == QMessageBox::Close) {
+                currentFile.clear();
+                ui -> textEdit -> clear();
+                setWindowTitle("Notepad");
+            } else {
+                return;
+            }
+        } else {
+            currentFile.clear();
+            ui -> textEdit -> clear();
+        }
+    } else {
+        currentFile.clear();
+        ui -> textEdit -> clear();
+    }
 }
 
 void mainwindow::save() {
-    return;
+    QString fileName;
+    if (currentFile.isEmpty()) {
+        fileName = QFileDialog::getSaveFileName(this, "Save");
+        if (fileName.isEmpty())
+            return;
+        // refresh our text in notepad text section
+        currentFile = fileName;
+    } else {
+        // if doesnt empty our file, assign our file to new filename
+        fileName = currentFile;
+    }
+
+    QFile file(fileName);
+
+    // if the file read only for our file or not text file
+    if (!file.open(QFile::WriteOnly | QFile::Text)) {
+        QMessageBox::warning(this, "Warning", "Cannot save file: " + file.errorString());
+        return;
+    }
+
+    // set the title of program to new file name
+    setWindowTitle(fileName);
+
+    QTextStream out(&file);
+    QString text = ui -> textEdit -> toPlainText();
+
+    out << text;
+
+    file.close();
 }
 
 void mainwindow::saveAs() {
+    QString fileName = QFileDialog::getSaveFileName(this, "Save As");
 
+    if (fileName.isEmpty()) return;
+
+    QFile file(fileName);
+
+    if (!file.open(QIODeviceBase::WriteOnly | QFile::Text)) {
+        QMessageBox::warning(this, "Warning", "Cannot save file: " + file.errorString());
+        return;
+    }
+
+    currentFile = fileName;
+
+    setWindowTitle(fileName);
+
+    QTextStream out(&file);
+    QString text = ui -> textEdit -> toPlainText();
+    out << text;
+
+    file.close();
 }
 
 void mainwindow::selectFont() {
@@ -88,10 +234,11 @@ void mainwindow::closeEvent(QCloseEvent *event) {
     QTextStream in(&file);
 
     if (!text.isEmpty()) {
-        if (!file.open(QIODevice::ReadOnly | QFile::Text)) {
+        if (!file.open(QIODeviceBase::ReadWrite)) {
             QMessageBox::warning(this, "Warning", "An error occured: " + file.errorString());
             return;
         }
+
         QString inText = in.readAll();
         if (text != inText) {
             QMessageBox::StandardButton reply = QMessageBox::question(
@@ -120,7 +267,6 @@ void mainwindow::cursorLoc() {
                     )
                 );
     QString fileName = QWidget::windowTitle();
-    if (!fileName.endsWith("~")) {
+    if (!fileName.endsWith("~"))
         this->setWindowTitle(fileName + "~");
-    }
 }
