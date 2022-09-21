7fd6621f2ea3ec20bac31642b4b97eb749ceaced
Seymen Kalkan <seymenkalkan@gmail.com>
2022-09-21 15:40:26 +0300

    Added build folder
    Added build directory of CMAKE.
    Compiled on x86_64 CPU.
    System is Manjaro with kernel version of 5.10.141.
    Compiler was GNU 12.2.0

---
+++ build/CMakeFiles/notepad-cpp.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for notepad-cpp.
+# This may be replaced when dependencies are built.
---
+++ build/notepad-cpp_autogen/include/ui_replacedialog.h
@@ -0,0 +1,87 @@
+/********************************************************************************
+** Form generated from reading UI file 'replacedialog.ui'
+**
+** Created by: Qt User Interface Compiler version 6.3.1
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_REPLACEDIALOG_H
+#define UI_REPLACEDIALOG_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QLineEdit>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_replaceDialog
+{
+public:
+    QLineEdit *txtFind;
+    QLabel *labelFind;
+    QLabel *labelReplace;
+    QLineEdit *txtReplace;
+    QPushButton *pushFind;
+    QPushButton *pushReplace;
+    QPushButton *pushRAll;
+    QLabel *countLabel;
+
+    void setupUi(QWidget *replaceDialog)
+    {
+        if (replaceDialog->objectName().isEmpty())
+            replaceDialog->setObjectName(QString::fromUtf8("replaceDialog"));
+        replaceDialog->resize(400, 173);
+        txtFind = new QLineEdit(replaceDialog);
+        txtFind->setObjectName(QString::fromUtf8("txtFind"));
+        txtFind->setGeometry(QRect(140, 30, 201, 28));
+        labelFind = new QLabel(replaceDialog);
+        labelFind->setObjectName(QString::fromUtf8("labelFind"));
+        labelFind->setGeometry(QRect(90, 30, 63, 21));
+        labelReplace = new QLabel(replaceDialog);
+        labelReplace->setObjectName(QString::fromUtf8("labelReplace"));
+        labelReplace->setGeometry(QRect(40, 70, 111, 21));
+        txtReplace = new QLineEdit(replaceDialog);
+        txtReplace->setObjectName(QString::fromUtf8("txtReplace"));
+        txtReplace->setGeometry(QRect(140, 70, 201, 28));
+        pushFind = new QPushButton(replaceDialog);
+        pushFind->setObjectName(QString::fromUtf8("pushFind"));
+        pushFind->setGeometry(QRect(70, 130, 87, 29));
+        pushReplace = new QPushButton(replaceDialog);
+        pushReplace->setObjectName(QString::fromUtf8("pushReplace"));
+        pushReplace->setGeometry(QRect(180, 130, 87, 29));
+        pushRAll = new QPushButton(replaceDialog);
+        pushRAll->setObjectName(QString::fromUtf8("pushRAll"));
+        pushRAll->setGeometry(QRect(290, 130, 87, 29));
+        countLabel = new QLabel(replaceDialog);
+        countLabel->setObjectName(QString::fromUtf8("countLabel"));
+        countLabel->setGeometry(QRect(140, 100, 201, 21));
+
+        retranslateUi(replaceDialog);
+
+        QMetaObject::connectSlotsByName(replaceDialog);
+    } // setupUi
+
+    void retranslateUi(QWidget *replaceDialog)
+    {
+        replaceDialog->setWindowTitle(QCoreApplication::translate("replaceDialog", "Replace", nullptr));
+        labelFind->setText(QCoreApplication::translate("replaceDialog", "Find", nullptr));
+        labelReplace->setText(QCoreApplication::translate("replaceDialog", "Replace with", nullptr));
+        pushFind->setText(QCoreApplication::translate("replaceDialog", "Find", nullptr));
+        pushReplace->setText(QCoreApplication::translate("replaceDialog", "Replace", nullptr));
+        pushRAll->setText(QCoreApplication::translate("replaceDialog", "Replace All", nullptr));
+        countLabel->setText(QString());
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class replaceDialog: public Ui_replaceDialog {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_REPLACEDIALOG_H
